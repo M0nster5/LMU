129b7e6a0b74b84d6972251658b74cca8dd98294
Connor Kordes <chkordes@gmail.com>
2015-07-01 10:19:28 +0200

    New: new class, works with multiple states

--- GillespieAlgorithm.cpp
+++ GillespieAlgorithm.cpp
@@ -12,18 +12,65 @@
 #include <fstream>
 #include <stdio.h>
 
+class RateStructure{
+private:
+    std::vector< std::vector< double > > rateMap;
+    std::vector<double> cummulativeRates;
+    double rSum;
+public:
+    RateStructure(){
+        rSum=0;
+    }
+    RateStructure(std::vector< std::vector< double > > r){
+        rateMap = r;
+        std::sort(rateMap.begin(),rateMap.end(), [](std::vector< double > r1, std::vector< double > r2){ return r1[1] > r2[1]; });
+        // I couldn't find how to make this more efficient
+        double sum = 0;
+        cummulativeRates.resize(rateMap.size());
+        for (int i = 0; i<rateMap.size();i++){
+            sum+=rateMap[i][1];
+            cummulativeRates[i] = sum;
+        }
+        rSum=sum;
+    }
+    
+    double find(double place){
+        for (int i = 0; i < cummulativeRates.size();i++){
+            if (place<cummulativeRates[i])
+                return rateMap[i][0];
+        }
+        return -1;
+    }
+    double getRSum(){return rSum;}
+    
+    void printRateMap(){
+        for (int x = 0; x < rateMap.size();x++){
+            for (int y = 0; y < rateMap[x].size();y++){
+                std::cout<<rateMap[x][y]<<" ";
+            }
+            std::cout<<"\n";
+        }
+    }
+    void printCummulativeRates(){
+        for (int x = 0; x<cummulativeRates.size();x++){
+            std::cout<<cummulativeRates[x]<<" ";
+        }
+        std::cout<<"\n";
+    }
+};
+
+
 class Gillepsie{
 private:
     double currentTime;
-    double position;
+    double positionX;
+    double positionY;
     double sum;
     int seed;
     int limit;
-    std::vector<double> rates;
-    std::vector<double> cummlativeRates;
+    RateStructure rs;
     std::vector< std::vector< double > > data;
-
-
+    
 public:
     Gillepsie(){
         currentTime = 0;
@@ -33,25 +80,25 @@ public:
     }
     
     
-    Gillepsie(std::vector<double> vec)
-      : rates(vec),
-       cummlativeRates(rates.size())
+    Gillepsie(std::vector< std::vector< double > > r)
+    : rs(r)
     {
-        rates = vec;
-        std::sort(rates.begin(),rates.end(), [](double r1, double r2){ return r1 > r2; });
-        std::partial_sum(rates.begin(), rates.end(), cummlativeRates.begin());
-        sum = cummlativeRates.back();
+        currentTime = 0;
+        seed = 0;
+        sum = 0;
+        limit = 100;
     }
     
     
-    Gillepsie(std::vector<double> vec, int s, int l)
-      : Gillepsie(vec)
+    Gillepsie(std::vector< std::vector< double > > vec, int s, int l)
+    : Gillepsie(vec)
     {
         seed = s;
         limit = l;
     }
     
     void run(){
+        double sum = rs.getRSum();
         double place = 0;
         double deltaT = 0;
         std::mt19937 mt_rand;
@@ -61,33 +108,54 @@ public:
             deltaT = (-log(die()))/sum;
             currentTime+=deltaT;
             place = die()*sum;
-            for (double i = 0; i<cummlativeRates.size();i++){
-                if (place<cummlativeRates[i]){
-                    if (i==1)
-                        position=position+1;
-                    else
-                        position=position-1;
-                    data.push_back({currentTime,i,position});
+            double vecPos = rs.find(place);
+            std::cout<< place<<" "<<vecPos<<"\n";
+            switch ( (int)vecPos )
+            {
+                case 1:
+                    positionY++;
                     break;
-                }
+                case 2:
+                    positionY--;
+                    break;
+                case 3:
+                    positionX++;
+                    break;
+                case 4:
+                    positionX--;
+                    break;
+                default:
+                    break;
             }
+            data.push_back({currentTime,positionX,positionY});
         }
-    }
-
-    void printCRates(){
-        for (int i = 0; i<rates.size();i++){
-            std::cout<<cummlativeRates[i] << '\t';
-        }
+        // for (double i = 0; i<cummlativeRates.size();i++){
+        //     if (place<cummlativeRates[i]){
+        //         if (i==1)
+        //             position=position+1;
+        //         else
+        //             position=position-1;
+        //         data.push_back({currentTime,i,position});
+        //         break;
+        //     }
+        // }
     }
     
-    void printRates(){
-        for (int i = 0; i<rates.size();i++){
-            std::cout<<rates[i] << '\t';
-        }
-    }
+    
+    // void printCRates(){
+    //     for (int i = 0; i<rates.size();i++){
+    //         std::cout<<cummlativeRates[i] << '\t';
+    //     }
+    // }
+    
+    // void printRates(){
+    //     for (int i = 0; i<rates.size();i++){
+    //         std::cout<<rates[i] << '\t';
+    //     }
+    // }
     
     void outputData(){
-        const char *path="/Users/connor/Desktop/C++:Python/Basic Gillespie Algorithm/output.txt";
+        const char *path="/Users/connor/Desktop/C++:Python/GillespieAlgorithm/output.txt";
         std::ofstream out_data(path);
         for (int x = 0; x<data.size();x++){
             for ( int y = 0; y<data[0].size();y++){
@@ -96,8 +164,9 @@ public:
             out_data<< '\n';
         }
     }
-    double getSum(){
-        return sum;
+    
+    RateStructure getRateStructure(){
+        return rs;
     }
 };
 
@@ -105,10 +174,12 @@ public:
 
 int main() {
     // insert code here...
-    std::vector<double> myRates{10,10};
-    Gillepsie myG(myRates, 1002, 10000);
+    std::vector< std::vector< double > > myRates{{1,10},{2,10},{3,10},{4,10}};
+    Gillepsie myG(myRates, 8877, 100);
+    myG.getRateStructure().printCummulativeRates();    
     myG.run();
     myG.outputData();
+    // myG.outputData();
     return 0;
 }
 
--- grapher.py
+++ grapher.py
@@ -1,9 +1,15 @@
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 import numpy as np
 
 file = open("output.txt",'r')
-t, p = np.loadtxt(file, usecols=(0, 2), unpack=True)
-plt.plot(t,p)
-plt.xlabel("Time")
-plt.ylabel("Position")
+t, x, y = np.loadtxt(file, usecols=(0, 1, 2), unpack=True)
+evenT = np.linspace(0,5,len(t))
+cmap = cm.jet
+fig = plt.figure(1)
+fig.clf()
+ax = fig.add_subplot(1,1,1)
+ax.scatter(x,y,c=evenT,cmap=cmap)
+plt.xlabel("X")
+plt.ylabel("Y")
 plt.show()
